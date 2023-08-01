5ec75524657408aac0e97aed0907fb35902dca68
Henry Tse <ht688@nyu.edu>
2023-08-01 11:30:38 -0400

    Update script.js
    added comments

--- script.js
+++ script.js
@@ -6,35 +6,40 @@ const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackb
 function search(str) {
 	let results = [];
 
-	// TODO
+	// .filter to meet criteria, case-insensitive 
 	results = fruit.filter(fruit => fruit.toLowerCase().includes(str.toLowerCase()));
 
 	return results;
 }
 
+// handles search bar input
 function searchHandler(e) {
-	// TODO
+	// case-insenstive input
 	const inputVal = e.target.value.toLowerCase();
 	const suggestion = search(inputVal);
 
 	showSuggestions(suggestion, inputVal);
 
+	// don't show suggestion if input is blank
 	if (!inputVal) {
 		suggestions.innerHTML = "";
 	}  
 }
 
 function showSuggestions(results, inputVal) {
-	// TODO
+	// clear suggestions for each input value
 	suggestions.innerHTML = "";
 
+	// loop over each suggestion to add bold text to same string as input
 	for (let res of results) {
+		// create new list element for each result
 		let newLi =  document.createElement("li");
 
 		let startIndex = res.toLowerCase().indexOf(inputVal);
+		let endIndex = startIndex + inputVal.length;
 		let boldRes = '';
 		
-		let endIndex = startIndex + inputVal.length; 
+		// slice part of string to add <strong> text to same string input
 		boldRes = res.slice(0, startIndex) 
 			+ '<strong>' + res.slice(startIndex, endIndex) + '</strong>'
 			+ res.slice(endIndex);
@@ -42,11 +47,13 @@ function showSuggestions(results, inputVal) {
 		newLi.innerHTML = boldRes;
 		suggestions.appendChild(newLi);
 		
+		// add highlight class to list item when mouseover
 		newLi.addEventListener("mouseover", function(e) {
 			closestLi = e.target.closest('li');
 			closestLi.classList.add('highlight');
 		});
 
+		// remove highlight class to list item when mouseout
 		newLi.addEventListener("mouseout", function(e) {
 			closestLi = e.target.closest('li');
 			closestLi.classList.remove('highlight');
@@ -55,7 +62,7 @@ function showSuggestions(results, inputVal) {
 }
 
 function useSuggestion(e) {
-	// TODO
+	// replaces input value with list item when clicked
 	closestLi = e.target.closest('li')
 	if (closestLi.classList.contains('highlight')) {
 		input.value = closestLi.textContent;
